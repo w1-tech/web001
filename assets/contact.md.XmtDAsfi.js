957df3c65a3f2a22900a815ab3e811f7655f9485
w1-tech <3885472228@qq.com>
2025-05-16 22:28:16 +0800

    Add files via upload

---
+++ assets/contact.md.XmtDAsfi.js
@@ -0,0 +1,11 @@
+import {_ as t, c as e, a1 as r, o} from "./chunks/framework.BE3BSss6.js";
+const f = JSON.parse('{"title":"联系我们","description":"","frontmatter":{},"headers":[],"relativePath":"contact.md","filePath":"contact.md"}')
+  , n = {
+    name: "contact.md"
+};
+function c(i, a, s, l, p, d) {
+    return o(),
+    e("div", null, a[0] || (a[0] = [r('<h1 id="联系我们" tabindex="-1">联系我们 <a class="header-anchor" href="#联系我们" aria-label="Permalink to &quot;联系我们&quot;">​</a></h1><h2 id="交流群" tabindex="-1">交流群 <a class="header-anchor" href="#交流群" aria-label="Permalink to &quot;交流群&quot;">​</a></h2><p>Minecraft1.21.1①群 730821147</p><p>Minecraft1.21.1②群 386483396</p><p>Minecraft1.21.1③群 452671272</p><p>Minecraft1.21.1④群 585764110</p><h2 id="其他方式" tabindex="-1">其他方式 <a class="header-anchor" href="#其他方式" aria-label="Permalink to &quot;其他方式&quot;">​</a></h2><p>举报联系QQ: 3110885374</p>', 8)]))
+}
+const m = t(n, [["render", c]]);
+export {f as __pageData, m as default};
